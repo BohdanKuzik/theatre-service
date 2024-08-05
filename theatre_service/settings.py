348e76ca51afacd7d1f8b6d0c21c063fc2da91a8
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-05 16:12:35 +0200

    add file requirements.txt

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'theatre',
+    'user',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
